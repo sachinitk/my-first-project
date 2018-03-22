8da6b6de95623fa10b0e45638be92bddf2531057
Sachin <sachinitk@gmail.com>
2018-03-22 17:05:07 +0530

    Update settings.py

--- demoproject1/settings.py
+++ demoproject1/settings.py
@@ -76,10 +76,8 @@ WSGI_APPLICATION = 'demoproject1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/etc/mysql/my.cnf',
-	        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 	}
 }
 
